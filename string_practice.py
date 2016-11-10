3813e986f4a53e902550f13a73354dcda217582c
Mary Elaine <marymahaffey1@gmail.com>
2016-11-10 16:07:22 -0500

    practice strings

---
+++ string_practice.py
@@ -0,0 +1,17 @@
+print 'Hello World'
+
+x = 'This is a string'
+print(x[0])
+print(x[0:1])
+print(x[0:2])
+print(x[-1])
+print(x[-4:-2])
+print(x[:3])
+print(x[3:])
+
+firstname='Christopher'
+lastname='Brooks'
+
+print(firstname + ' ' + lastname)
+print(firstname * 3)
+print('Chris' in firstname)
