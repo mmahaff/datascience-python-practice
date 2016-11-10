4246a07527861ed65c35be284640b6b8cb42db3c
Mary Elaine <marymahaffey1@gmail.com>
2016-11-10 16:21:33 -0500

    Finished string exercise

--- string_practice.py
+++ string_practice.py
@@ -15,3 +15,16 @@ lastname='Brooks'
 print(firstname + ' ' + lastname)
 print(firstname * 3)
 print('Chris' in firstname)
+
+print('Chris' + str(2))
+
+sales_record = {'price': 3.24,
+                'num_items': 4,
+                'person': 'Chris'}
+sales_statement = '{} bought {} items at a price of {} each for a total of {}'
+
+print(sales_statement.format(sales_record['person'],
+                             sales_record['num_items'],
+                             sales_record['price'],
+                             sales_record['num_items']*sales_record['price']))
+                             
